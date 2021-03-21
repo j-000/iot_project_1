dfd21ffeb23e1a070a63faa22001beeae1cf696d
j-000 <jjasilva85@gmail.com>
2021-03-21 13:40:32 +0000

    Refactored credentials to a file;

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,38 @@
+// Custom files
 #include <ATcommands.h>
 #include <UtilFunctions.h>
-#include <stdlib.h>
+#include <credentials.h>     // Contains SSID and PWD for wifi.
 
+// Libraries
 #include<Arduino.h>
 #include <SoftwareSerial.h>
-#include <WiFi.h>
 
-
+// TX and RX pins definitions
 #define TX_PIN 2
 #define RX_PIN 3
 
-
+// Global variables
 SoftwareSerial ESPSerial(TX_PIN, RX_PIN);
-
-
-
-// Wifi Module
-String SSID = "\"\"";
-String PWD = "\"\"";
 String host = "\"http://joalex.dev\"";
 String port = "\"80\"";
 String protocol = "\"TCP\"";
-int oneMinute = 30000;
+int oneMinute = 60000; 
 
-
-WiFiClient client;
-
-// Temperature Module
+// Thermistor pin and resistor definition
 int thermistorPin = A0;
 float thermistorResistor = 100000;
 
 
-
-
 void setup() {
+  // Start Serial and ESPSerial
   Serial.begin(9600);
   ESPSerial.begin(9600); 
-  
-  espRunCommand(ESPSerial, AT_RESET, 1000);                                // Reset Chip
-  espRunCommand(ESPSerial, AT_SET_STATION_MODE, 1000);                     // Set station mode
-  espRunCommand(ESPSerial, AT_CONNECT_TO_WIFI + SSID + "," + PWD, 1000);   // Connect to wifi
-  
+  // Reset ESP
+  espRunCommand(ESPSerial, AT_RESET, 1000);
+  // Set Station Mode
+  espRunCommand(ESPSerial, AT_SET_STATION_MODE, 1000);
+  // Connect to wifi
+  espRunCommand(ESPSerial, AT_CONNECT_TO_WIFI + SSID + "," + PWD, 1000);
   delay(1000);
 }
 
@@ -55,25 +47,22 @@ void loop(){
   }
   
   // Manual input from Serial Monitor
-  if (Serial.available()){ESPSerial.write(Serial.read());}   
+  if (Serial.available()){
+    ESPSerial.write(Serial.read());
+  }   
   
-  
+ if (millis() % oneMinute == 0){
+    // Test connection and reset if wifi is off
+    boolean wifiIsOn = testWifiConnection(ESPSerial);
 
-//  if (millis() % oneMinute == 0){
-//
-//    // Test connection and reset if wifi is off
-//    boolean wifiIsOn = testWifiConnection(ESPSerial);
-//
-//    if (wifiIsOn){
-//      // Get temperature
-//      String temperature = getTemperature();
-//      Serial.println("\nTemperature: " + temperature);
-//    }else{
-//      resetWifi(ESPSerial);
-//    }
-// 
-//  }
-  
+    if (wifiIsOn){
+      // Get temperature
+      String temperature = getTemperature(thermistorPin, thermistorResistor);
+      Serial.println("\nTemperature: " + temperature);
+    }else{
+      resetWifi(ESPSerial);
+    }
+  }
 }
 
 
